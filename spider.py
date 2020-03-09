387536e84dc20f537d850bb64266ad54c3bb2aea
Luiz Guizi <32824395+lguizi@users.noreply.github.com>
2020-03-08 22:13:55 -0300

    Update spider.py

--- spider.py
+++ spider.py
@@ -3,28 +3,28 @@ from bs4 import BeautifulSoup
 from colorama import *
 
 class spider:
-	def __init__(self,url):
-		print ("\n {}> SPIDER".format(Fore.YELLOW))
-		self.url_master = url
-		self.links_extraidos = []
-		self.links_internos = []
-		self.links_externos = []
-	def extrair(self,url):
-		print ('      |-[{}]                                                '.format(url))
-		conteudo = BeautifulSoup(requests.get(url).content,'lxml')
-		links = conteudo.findAll(href=True)
-		for link in links:
-			self.links_extraidos.append(str(link['href']))
-			for link in self.links_extraidos:
-				if 'http' not in link and link[:1] != '/':
-					link = self.url_master+'/'+link
-				elif 'http' not in link and link[:1] == '/':
-					link = self.url_master+link
-				if self.url_master in link and link not in self.links_internos:
-					self.links_internos.append(link)
-					print ('          |-{}'.format(link))
-				elif self.url_master not in link and link not in self.links_externos:
-					self.links_externos.append(link)
-	def resumo(self):
-		print ('        |------ {} INTERNAL LINK(S)'.format(len(self.links_internos)))
-		print ('        |------ {} EXTERNAL LINK(S)'.format(len(self.links_externos)))
+    def __init__(self,url):
+        print (f"\n {Fore.YELLOW}> SPIDER")
+        self.url_master = url
+        self.links_extraidos = self.links_internos = self.links_externos = []
+
+        def extrair(self,url):
+            print (' '*6 + f'|-[{url}]' + ' '*48)
+            conteudo = BeautifulSoup(requests.get(url).content,'lxml')
+            links = conteudo.findAll(href=True)
+
+            for link in links:
+                self.links_extraidos.append(str(link['href']))
+                for link in self.links_extraidos:
+                    if 'http' not in link and link[:1] != '/':
+                        link = self.url_master+'/'+link
+                    elif 'http' not in link and link[:1] == '/':
+                        link = self.url_master+link
+                    if self.url_master in link and link not in self.links_internos:
+                        self.links_internos.append(link)
+                        print (f'          |-{link}')
+                    elif self.url_master not in link and link not in self.links_externos:
+                        self.links_externos.append(link)
+        def resumo(self):
+            print (' '*8 + f'|------ {len(self.links_internos)} INTERNAL LINK(S)')
+            print (' '*8 + f'|------ {len(self.links_externos)} EXTERNAL LINK(S)')

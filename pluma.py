8bc29a10f66f14b40122513128a48b3610ea61ff
Luiz Guizi <32824395+lguizi@users.noreply.github.com>
2020-03-08 22:08:36 -0300

    Update pluma.py

--- pluma.py
+++ pluma.py
@@ -6,30 +6,31 @@ import time
 from pyfiglet import Figlet
 
 try:
-	f = Figlet(font='roman')
-	print (f.renderText('Pluma'))
-	print ("|-Version: 0.3")
-	print ("|-Author: Lucas Antoniaci")  
-	print ("_____________________________________________________________________________________________________________________________")
-	url = url(input(" > Insert a URL for analysis: "))
-	if url.verificaUrl():
-		now = datetime.now()
-		inicio = time.time()
-		print (" > DATA {}".format(now.strftime("%d/%m/%Y %H:%M:%S")))
-		scan = scan(url)
-		scan.info()
-		scan.heads()
-		scan.cookies()
-		scan.methods()
-		scan.autocomplete()
-		scan.enum()
-		spider = spider(url.url)
-		spider.extrair(spider.url_master)
-		i = 1
-		for link in spider.links_internos:
-			spider.extrair(link)
-			i += 1
-		fim = time.time()
-		print ("\n\nAnd...Done! :) [{} secs]".format(int(fim - inicio)))
+    f = Figlet(font='roman')
+    print (f.renderText('Pluma'))
+    print ("|-Version: 0.3")
+    print ("|-Author: Lucas Antoniaci")
+    print ("_"*125)
+    url = url(input(" > Insert a URL for analysis: "))
+
+    if url.verificaUrl():
+        now = datetime.now()
+        inicio = time.time()
+        print (" > DATA {}".format(now.strftime("%d/%m/%Y %H:%M:%S")))
+        scan = scan(url)
+        scan.info()
+        scan.heads()
+        scan.cookies()
+        scan.methods()
+        scan.autocomplete()
+        scan.enum()
+        spider = spider(url.url)
+        spider.extrair(spider.url_master)
+        i = 1
+        for link in spider.links_internos:
+            spider.extrair(link)
+            i += 1
+        fim = time.time()
+        print (f"\n\nAnd...Done! :) [{int(fim - inicio)} secs]")
 except (KeyboardInterrupt, SystemExit):
-        print ("\nEXECUTION STOPPED!")
+    print ("\nEXECUTION STOPPED!")
